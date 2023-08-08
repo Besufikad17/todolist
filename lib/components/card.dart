637286af371666d66fb27a395f716b4b830a7f15
besufikad17 <besufikadtilahun1994@gmail.com>
2023-08-08 17:44:59 +0300

    first commit

---
+++ lib/components/card.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MyCard());
+
+class MyCard extends StatelessWidget {
+  const MyCard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
